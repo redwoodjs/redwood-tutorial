268a26f6c0cf3971a7b6c10d9dbddc3845c044eb
Rob Cameron <rob.cameron@fastmail.com>
2020-10-19 12:08:43 -0700

    Include createdAt in blog posts to prevent Storybook error

--- web/src/components/BlogPost/BlogPost.stories.js
+++ web/src/components/BlogPost/BlogPost.stories.js
@@ -7,6 +7,7 @@ export const generated = () => {
         id: 1,
         title: 'First Post',
         body: 'Neutra tacos hot chicken prism raw denim...',
+        createdAt: '2020-01-01T12:34:56Z',
       }}
     />
   )
--- web/src/components/BlogPostsCell/BlogPostsCell.mock.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.mock.js
@@ -6,12 +6,14 @@ export const standard = (/* vars, { ctx, req } */) => ({
       title: 'First post',
       body:
         'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.',
+      createdAt: '2020-01-01T12:34:56Z',
     },
     {
       id: 2,
       title: 'Second post',
       body:
         'Master cleanse gentrify irony put a bird on it hexagon enamel pin. Pop-up man braid artisan pug tilde synth lo-fi. Ethical tofu portland keytar waistcoat. Pabst authentic hammock chillwave twee trust fund. Lyft humblebrag ramps irony unicorn.',
+      createdAt: '2020-01-01T12:34:56Z',
     },
   ],
 })
