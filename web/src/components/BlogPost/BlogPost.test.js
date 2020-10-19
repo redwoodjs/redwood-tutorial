599b61a64ea3d336aad983dc47e4814823dfbd9a
Rob Cameron <rob.cameron@fastmail.com>
2020-10-19 16:00:02 -0700

    Fix BlogPost test

--- web/src/components/BlogPost/BlogPost.test.js
+++ web/src/components/BlogPost/BlogPost.test.js
@@ -1,11 +1,18 @@
-import { render } from '@redwoodjs/testing'
+import { render, screen } from '@redwoodjs/testing'
 
 import BlogPost from './BlogPost'
 
 describe('BlogPost', () => {
-  it('renders successfully', () => {
-    expect(() => {
-      render(<BlogPost post={{ id: 1, name: 'John Doe', body: 'Body' }} />)
-    }).not.toThrow()
+  it('renders a blog post', () => {
+    const post = {
+      id: 1,
+      title: 'First post',
+      body: 'Neutra tacos hot chicken prism raw denim...',
+      createdAt: new Date().toISOString(),
+    }
+    render(<BlogPost post={post} />)
+
+    expect(screen.getByText(post.title)).toBeInTheDocument()
+    expect(screen.getByText(post.body)).toBeInTheDocument()
   })
 })
