80ec8b52ea7611dde9dc71ee228268dc9706687b
Rob Cameron <rob.cameron@fastmail.com>
2020-10-20 16:22:49 -0700

    Full text for BlogPost test

--- web/src/components/BlogPost/BlogPost.test.js
+++ web/src/components/BlogPost/BlogPost.test.js
@@ -7,7 +7,7 @@ describe('BlogPost', () => {
     const post = {
       id: 1,
       title: 'First post',
-      body: 'Neutra tacos hot chicken prism raw denim...',
+      body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
       createdAt: new Date().toISOString(),
     }
     render(<BlogPost post={post} />)
