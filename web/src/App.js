5c7f9832f989967504f59dd346f430631e9b88e1
David S Price <thedavid@thedavidprice.com>
2021-03-10 23:26:51 -0800

    upgrade and code mods v0.26 and v0.27

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,25 @@
 import { AuthProvider } from '@redwoodjs/auth'
 import netlifyIdentity from 'netlify-identity-widget'
-import ReactDOM from 'react-dom'
 import { FatalErrorBoundary } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
+import { isBrowser } from '@redwoodjs/prerender/browserUtils'
 
 import Routes from 'src/Routes'
 
 import './scaffold.css'
 import './index.css'
 
-netlifyIdentity.init()
+isBrowser && netlifyIdentity.init()
 
-ReactDOM.render(
+const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <AuthProvider client={netlifyIdentity} type="netlify">
       <RedwoodApolloProvider>
         <Routes />
       </RedwoodApolloProvider>
     </AuthProvider>
-  </FatalErrorBoundary>,
-  document.getElementById('redwood-app')
+  </FatalErrorBoundary>
 )
+
+export default App
