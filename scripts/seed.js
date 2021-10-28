1ce41a4260aeb5801f228357d3a05de9cad3e48e
David Price <thedavid@thedavidprice.com>
2021-10-27 19:34:59 -0700

    v0.38.0 with code mods

---
+++ scripts/seed.js
@@ -0,0 +1,44 @@
+import { db } from '$api/src/lib/db'
+
+export default async () => {
+  try {
+    //
+    // Manually seed via `yarn rw prisma db seed`
+    // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
+    //
+    // Update "const data = []" to match your data model and seeding needs
+    //
+    const POSTS = [
+      {
+        title: 'Welcome to the blog!',
+        body: "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
+      },
+      {
+        title: 'A little more about me',
+        body: "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
+      },
+      {
+        title: 'What is the meaning of life?',
+        body: 'Meh waistcoat succulents umami asymmetrical, hoodie post-ironic paleo chillwave tote bag. Trust fund kitsch waistcoat vape, cray offal gochujang food truck cloud bread enamel pin forage. Roof party chambray ugh occupy fam stumptown. Dreamcatcher tousled snackwave, typewriter lyft unicorn pabst portland blue bottle locavore squid PBR&B tattooed.',
+      },
+    ]
+
+    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
+    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
+    Promise.all(
+      //
+      // Change to match your data model and seeding needs
+      //
+      POSTS.map(async (post) => {
+        const record = await db.post.create({
+          data: { title: post.title, body: post.body },
+        })
+
+        console.log(record)
+      })
+    )
+  } catch (error) {
+    console.warn('Please define your seed data.')
+    console.error(error)
+  }
+}
