eefeab1bf0242bb7d1741642de9894b7d673e49b
David Corbitt <davidlcorbitt@gmail.com>
2022-01-18 18:14:11 -0700

    Upgrade Tailwindcss to v3

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['src/**/*.{js,jsx,ts,tsx}'],
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
