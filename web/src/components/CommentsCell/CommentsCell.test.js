0189c27d1660cc9f28e864a19b4a7b96d7ddda18
Rob Cameron <rob.cameron@fastmail.com>
2022-02-10 14:26:20 -0800

    Adds backend for comments

--- web/src/components/CommentsCell/CommentsCell.test.js
+++ web/src/components/CommentsCell/CommentsCell.test.js
@@ -10,9 +10,8 @@ describe('CommentsCell', () => {
   })
 
   it('renders Empty successfully', async () => {
-    expect(() => {
-      render(<Empty />)
-    }).not.toThrow()
+    render(<Empty />)
+    expect(screen.getByText('No comments yet')).toBeInTheDocument()
   })
 
   it('renders Failure successfully', async () => {
