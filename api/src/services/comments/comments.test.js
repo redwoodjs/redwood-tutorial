c9c26a8b947d71d16f050b14e22244eb1c39f9e5
ronatory <ronatory@gmail.com>
2022-04-14 11:56:21 +0200

    Finish part adding comments to the schema

---
+++ api/src/services/comments/comments.test.js
@@ -0,0 +1,30 @@
+import { comments, createComment } from './comments'
+
+// Generated boilerplate tests do not account for all circumstances
+// and can fail without adjustments, e.g. Float and DateTime types.
+//           Please refer to the RedwoodJS Testing Docs:
+//       https://redwoodjs.com/docs/testing#testing-services
+// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+
+describe('comments', () => {
+  scenario('returns all comments', async (scenario) => {
+    const result = await comments()
+
+    expect(result.length).toEqual(Object.keys(scenario.comment).length)
+  })
+
+  scenario('postOnly', 'creates a new comment', async (scenario) => {
+    const comment = await createComment({
+      input: {
+        name: 'Billy Bob',
+        body: 'What is your favorite tree bark?',
+        postId: scenario.post.bark.id,
+      },
+    })
+
+    expect(comment.name).toEqual('Billy Bob')
+    expect(comment.body).toEqual('What is your favorite tree bark?')
+    expect(comment.postId).toEqual(scenario.post.bark.id)
+    expect(comment.createdAt).not.toEqual(null)
+  })
+})
