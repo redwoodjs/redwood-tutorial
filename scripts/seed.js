e77cdd0e546ee9b91cf5f160dd12d62bd2116ab1
Rob Cameron <cannikin@fastmail.com>
2022-09-29 13:42:36 -0700

    Reorder seeds

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,37 +1,24 @@
 import { db } from 'api/src/lib/db'
 
+const POSTS = [
+  {
+    id: 1,
+    title: 'Welcome to the blog!',
+    body: "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
+  },
+  {
+    id: 2,
+    title: 'A little more about me',
+    body: "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
+  },
+  {
+    id: 3,
+    title: 'What is the meaning of life?',
+    body: 'Meh waistcoat succulents umami asymmetrical, hoodie post-ironic paleo chillwave tote bag. Trust fund kitsch waistcoat vape, cray offal gochujang food truck cloud bread enamel pin forage. Roof party chambray ugh occupy fam stumptown. Dreamcatcher tousled snackwave, typewriter lyft unicorn pabst portland blue bottle locavore squid PBR&B tattooed.',
+  },
+]
+
 export default async () => {
-  const POSTS = [
-    {
-      id: 1,
-      title: 'Welcome to the blog!',
-      body:
-        "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
-    },
-    {
-      id: 2,
-      title: 'A little more about me',
-      body:
-        "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
-    },
-    {
-      id: 3,
-      title: 'What is the meaning of life?',
-      body:
-        'Meh waistcoat succulents umami asymmetrical, hoodie post-ironic paleo chillwave tote bag. Trust fund kitsch waistcoat vape, cray offal gochujang food truck cloud bread enamel pin forage. Roof party chambray ugh occupy fam stumptown. Dreamcatcher tousled snackwave, typewriter lyft unicorn pabst portland blue bottle locavore squid PBR&B tattooed.',
-    },
-  ]
-
-  for (const post of POSTS) {
-    await db.post.upsert({
-      where: { id: post.id },
-      create: { id: post.id, title: post.title, body: post.body },
-      update: {},
-    })
-
-    console.log(`  Seeded "${post.title}"`)
-  }
-
   // create an admin user
   await db.user.upsert({
     where: { id: 1 },
@@ -44,6 +31,16 @@ export default async () => {
     },
     update: {},
   })
+
+  for (const post of POSTS) {
+    await db.post.upsert({
+      where: { id: post.id },
+      create: { id: post.id, title: post.title, body: post.body },
+      update: {},
+    })
+
+    console.log(`  Seeded "${post.title}"`)
+  }
 
   console.info('')
   console.info('  Seeded admin user:')
