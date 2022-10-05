a4e7e14d5801057cbd21aeefd211296a2dc9550a
Rob Cameron <cannikin@fastmail.com>
2022-10-05 16:24:16 -0700

    Chapter 5 changes

--- web/src/components/Article/Article.stories.js
+++ web/src/components/Article/Article.stories.js
@@ -1,17 +1,17 @@
 import Article from './Article'
 
-export const generated = () => {
-  return (
-    <Article
-      article={{
-        id: 1,
-        title: 'First Post',
-        body:
-          'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.',
-        createdAt: '2020-01-01T12:34:56Z',
-      }}
-    />
-  )
+const ARTICLE = {
+  id: 1,
+  title: 'First Post',
+  body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+}
+
+export const full = () => {
+  return <Article article={ARTICLE} />
+}
+
+export const summary = () => {
+  return <Article article={ARTICLE} summary={true} />
 }
 
 export default { title: 'Components/Article' }
