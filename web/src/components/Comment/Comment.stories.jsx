d90478337fe6d15611890edf7d033563c2558fcf
mythreya75 <mythreyav09@gmail.com>
2023-09-24 20:57:39 -0400

    Built comment component

---
+++ web/src/components/Comment/Comment.stories.jsx
@@ -0,0 +1,29 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```jsx
+// export const Primary = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import Comment from './Comment'
+
+export const generated = () => {
+  return (
+    <Comment
+      comment={{
+        name: 'Rob Cameron',
+        body: 'This is the first comment!',
+        createdAt: '2020-01-01T12:34:56Z'
+      }}
+    />
+  )
+}
+
+export default { component: Comment }
+
+export const Primary = {}
