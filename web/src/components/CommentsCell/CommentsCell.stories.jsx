6dc8e1de03ff8812c81c66c2424ac429302ce710
mythreya75 <mythreyav09@gmail.com>
2023-09-24 21:13:39 -0400

    multiple comments

---
+++ web/src/components/CommentsCell/CommentsCell.stories.jsx
@@ -0,0 +1,32 @@
+import { Loading, Empty, Failure, Success } from './CommentsCell'
+import { standard } from './CommentsCell.mock'
+
+const meta = {
+  title: 'Cells/CommentsCell',
+}
+
+export default meta
+
+export const loading = {
+  render: () => {
+    return Loading ? <Loading /> : <></>
+  },
+}
+
+export const empty = {
+  render: () => {
+    return Empty ? <Empty /> : <></>
+  },
+}
+
+export const failure = {
+  render: (args) => {
+    return Failure ? <Failure error={new Error('Oh no')} {...args} /> : <></>
+  },
+}
+
+export const success = {
+  render: (args) => {
+    return Success ? <Success {...standard()} {...args} /> : <></>
+  },
+}
